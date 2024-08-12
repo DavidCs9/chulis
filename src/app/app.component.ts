0469ea967d2b8c495e504359bfcfb902639719f5
DavidCs9 <davidcastrosi@hotmail.com>
2024-08-12 17:08:46 -0600

    feat: Add monthverse message and days since counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'chulis';
   cartaDialog!: HTMLDialogElement;
+  startDate = '2024-06-12';
+  daysSince = 0;
+  isMonthverse = false;
 
   ngOnInit(): void {
     this.cartaDialog = document.querySelector('#carta')!;
+    this.calculateDaysSince();
+    this.calculateIsMonthverse();
   }
 
   openCarta() {
@@ -24,5 +29,24 @@ export class AppComponent implements OnInit {
 
   closeCarta() {
     this.cartaDialog.close();
+  }
+
+  calculateDaysSince() {
+    const startDate = new Date(this.startDate);
+    const currentDate = new Date();
+    const timeDifference = currentDate.getTime() - startDate.getTime();
+    const daysSince = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    this.daysSince = daysSince;
+  }
+
+  calculateIsMonthverse() {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    const dayNaverse = 12;
+    if (currentDay === dayNaverse) {
+      this.isMonthverse = true;
+    } else {
+      this.isMonthverse = false;
+    }
   }
 }
